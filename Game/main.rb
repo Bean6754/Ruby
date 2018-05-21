c1b257c914d330d9c5ebc919d8d21dede78206aa
Matthew <Bean6754@users.noreply.github.com>
2018-05-21 14:43:02 +0100

    Update main.rb

--- Game/main.rb
+++ Game/main.rb
@@ -1,13 +1,18 @@
 #!/usr/bin/env ruby
 
-if File.exist?("gamedata.txt")
-	puts "Game data found, not overwriting."
+# Functions.
+def introduction
+  if File.exist?("gamedata.txt")
+	  puts "Game data found, not overwriting."
 
-	contents = File.read('gamedata.txt')
-	puts "Welcome back, " + contents + "!"
-else
-	puts "What is your name? "
-	name = gets.chomp
-	File.open('gamedata.txt', 'a') { |f| f.write(name) }
-	puts "Welcome " + name + "."
+	  contents = File.read('gamedata.txt')
+	  puts "Welcome back, " + contents + "!"
+  else
+	  puts "What is your name? "
+	  name = gets.chomp
+	  File.open('gamedata.txt', 'a') { |f| f.write(name) }
+	  puts "Welcome " + name + "."
+  end
 end
+
+introduction
